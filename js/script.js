4d3aeada81f52e055a24a26ed1e835a149264c9e
Keila <keiladaianefernandes24@gmail.com>
2019-08-29 19:30:08 -0300

    Farol esquerdo adicionado, com a rotação bugada dele

--- js/script.js
+++ js/script.js
@@ -4,23 +4,44 @@ let canvas = document.getElementById("tela")
 let ctx = canvas.getContext("2d")
 
 // variáveis
-let x = 700, y = 350, larg = 30, alt = 50, ang = 0
+let velocidade = 5
+let x = 700,
+    y = 350,
+    larg = 50,
+    alt = 90,
+    ang = 0
+let xFarolEsquerdo = x - larg + 30,
+    yFarolEsquerdo = y - alt + 48,
+    largFarolEsquerdo = 7,
+    altFarolEsquerdo = 5,
+    angFarolEsquerdo = 0
 
 // inicia um vetor de 256 teclas
 let teclas = []
-for(let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {
     teclas[i] = false
 }
 
-function desenhar(){
+function desenhar() {
     processaTeclas()
-    // carro
+
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height)
+    ctx.save()
+
+    // carro
     ctx.fillStyle = "rgb(255, 0, 0)"
     ctx.save()
     ctx.translate(x, y)
     ctx.rotate(ang)
-    ctx.fillRect(-larg/2, -alt/2, larg, alt)
+    ctx.fillRect(-larg / 2, -alt / 2, larg, alt)
+    ctx.restore()
+
+    // farol esquerdo
+    ctx.fillStyle = "rgb(255, 255, 255)"
+    ctx.save()
+    ctx.translate(xFarolEsquerdo, yFarolEsquerdo)
+    ctx.rotate(ang)
+    ctx.fillRect(-largFarolEsquerdo / 2, -altFarolEsquerdo / 2, largFarolEsquerdo, altFarolEsquerdo)
     ctx.restore()
 
     requestAnimationFrame(desenhar)
@@ -28,21 +49,46 @@ function desenhar(){
 
 
 // movimentação
-document.onkeydown = (evt) => { teclas[evt.keyCode] = true }
+document.onkeydown = (evt) => {
+    teclas[evt.keyCode] = true
+}
 
-document.onkeyup = (evt) => { teclas[evt.keyCode] = false }
+document.onkeyup = (evt) => {
+    teclas[evt.keyCode] = false
+}
 
-function processaTeclas(){
+function processaTeclas() {
     // Movimentação do carro
     if (teclas[38]) { // cima - frente
-        y -= Math.cos(ang) * 5
-        x += Math.sin(ang) * 5
-    }  if (teclas[40]) { // baixo - trás
-        y += 5
-    }  if (teclas[39]) { // direita
-        ang += 0.1
-    }  if (teclas[37] ) { // esquerda
-        ang -= 0.1
+        // carro
+        x += Math.sin(ang) * velocidade
+        y -= Math.cos(ang) * velocidade
+
+        // farol esquerdo
+        xFarolEsquerdo += Math.sin(ang) * velocidade
+        yFarolEsquerdo -= Math.cos(ang) * velocidade
+
+
+    }
+    if (teclas[40]) { // baixo - trás
+        // carro
+        y += velocidade
+
+        // farol esquerdo
+        yFarolEsquerdo += velocidade
+
+    }
+    if (teclas[39]) { // direita
+        // carro
+        ang += 0.01
+
+
+    }
+    if (teclas[37]) { // esquerda
+        // carro
+        ang -= 0.01
+        
+
     }
 }
 
